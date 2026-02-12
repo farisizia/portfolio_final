507c18f722a8e4f117160f6a742f3ba37e224fdf
Faris Izzi asrori <64596666+farisizia@users.noreply.github.com>
2026-02-12 11:49:04 +0700

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                primary: '#38bdf8', // Sky 400 (lighter for dark mode)
+                secondary: '#94a3b8', // Slate 400
+                dark: '#0f172a', // Slate 900
+                darker: '#020617', // Slate 950
+                card: '#1e293b', // Slate 800
+            }
+        }
+    }
+}
